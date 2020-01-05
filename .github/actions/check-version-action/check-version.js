44f0f9b33d90948f3fefa53367c18ab8d9968240
riQQ <riQQ@users.noreply.github.com>
2020-01-05 20:04:07 +0100

    Fix syntax errors in check-version.js

--- .github/actions/check-version-action/check-version.js
+++ .github/actions/check-version-action/check-version.js
@@ -4,9 +4,9 @@ const core = require('@actions/core');
 
 (async () => {
   try {
-    const response = await got('https://mtgarena.downloads.wizards.com/Live/Windows32/version')
+    const response = await got('https://mtgarena.downloads.wizards.com/Live/Windows32/version');
     console.log(`The respsone: ${response.body}`);
   } catch (error) {
     core.setFailed(error.message);
   }
-}
+})();
