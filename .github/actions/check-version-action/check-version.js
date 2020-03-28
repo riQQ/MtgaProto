6c27825a0c8a864a96a11a0fa30e2de9ee67871d
riQQ <riQQ@users.noreply.github.com>
2020-03-28 15:34:58 +0100

    Print versions

--- .github/actions/check-version-action/check-version.js
+++ .github/actions/check-version-action/check-version.js
@@ -6,6 +6,8 @@ const core = require('@actions/core');
   try {
     const response = await got('https://mtgarena.downloads.wizards.com/Live/Windows32/version');
     console.log(`The response: ${response.body}`);
+    const responseJson = JSON.parse(response.body);
+    console.log(`Versions: ${JSON.stringify(responseJson.Versions)}`);
   } catch (error) {
     core.setFailed(error.message);
   }
