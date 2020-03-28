3f0ef682c406b0e028bba3afe19a6b6596ea3deb
riQQ <riQQ@users.noreply.github.com>
2020-03-28 15:19:07 +0100

    Fix typo in string

--- .github/actions/check-version-action/check-version.js
+++ .github/actions/check-version-action/check-version.js
@@ -5,7 +5,7 @@ const core = require('@actions/core');
 (async () => {
   try {
     const response = await got('https://mtgarena.downloads.wizards.com/Live/Windows32/version');
-    console.log(`The respsone: ${response.body}`);
+    console.log(`The response: ${response.body}`);
   } catch (error) {
     core.setFailed(error.message);
   }
